85c47dcb6e8067abf6f089341a15bf53127a7fa1
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-20 14:38:57 -0600

    update10

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector("button")
+
+buyButton = () => {
+    alert("Siiiiike you don't got the power for this sweet baby! LOL")
+}
+
+button.addEvevntListener('click', buyButton)
