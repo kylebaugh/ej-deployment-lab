9335a2dd3201a7bb267a88774aff70b2199bc8fd
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-20 13:37:25 -0600

    update1

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 4005
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../page.html'))
+}
+)
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
 })
 
 app.listen(port, () =>{
