8d0b76e72168f1d27ee2eca5adbec27aa7956a1c
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-20 14:53:54 -0600

    update15

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ buyButton = () => {
     alert("Siiiiike you don't got the power for this sweet baby! LOL")
 }
 
-button.addEvevntListener('click', buyButton)
+button.addEventListener('click', buyButton)
