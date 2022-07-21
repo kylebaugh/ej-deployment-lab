c0129039fdfdf837be4384da55a7e8e09275dd64
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-21 13:22:52 -0600

    update

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const Rollbar = require("rollbar")
+
 let button = document.querySelector("#buy")
 
 buyButton = () => {
+    rollbar.log('you got trolled')
     alert("Siiiiike you don't got the power for this sweet baby! LOL")
 }
 
